dd2db4428a69e2c6d53fa62c3faede95e1f4f5e5
shreyoshi <shreyoshimahato@gmail.com>
2017-03-06 20:55:02 +0530

    admin  dao changed

--- WanderLust/src/com/mmt/model/dao/AdminDaoImplMMT.java
+++ WanderLust/src/com/mmt/model/dao/AdminDaoImplMMT.java
@@ -1,91 +1,109 @@
 package com.mmt.model.dao;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-
+//import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import com.mmt.model.bean.Admin;
 
 
 public class AdminDaoImplMMT implements AdminDao{
-	
+	Configuration cfg=new Configuration();
 	//insert function for the admin board
 	
 	@Override
 	public int insert(Admin admin) throws SQLException, ClassNotFoundException, IOException {
-		int row;
-		Connection con=DbConnection.dbConnection();
-		PreparedStatement pst=con.prepareStatement("insert into admin values(?,?,?,?,?,?)");
-		pst.setString(1, admin.getAdminId());
-		pst.setString(2,admin.getAdminName());
-		pst.setLong(3,admin.getAdminPhoneNo());
-		pst.setString(4,admin.getAdminEmailId());
-		pst.setString(5,admin.getAdminAddress());
-		pst.setString(6,admin.getAdminPassword());
-		
-		
-		row=pst.executeUpdate();
-		con.close();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory factory=cfg.buildSessionFactory();
+		Session session=factory.openSession();
+		Transaction tx=null;
+		int row=0;
+		try{
+			tx=session.beginTransaction();
+			session.save(admin);
+			tx.commit();
+			System.out.println("Record Inserted");
+			row=1;
+			session.close();
+			return row;
+			}
+		catch(Exception ex){
+			tx.rollback();
+		}
+		session.close();
 		return row;
 	}
 
 		// search function for admin
 	@Override
-	public Admin search(String uid) throws SQLException, ClassNotFoundException, IOException {
-		Admin admin=new Admin();
-		ResultSet rs;
-		Connection con=DbConnection.dbConnection();;
-		PreparedStatement pst=con.prepareStatement("select * from admin where ADMINID=?");
-		pst.setString(1, uid);
-		rs=pst.executeQuery();
-		
-		if(rs.next()){
-		
-			admin.setAdminId((rs.getString("ADMINID")));
-			admin.setAdminName((rs.getString("ADMINNAME")));
-			admin.setAdminPhoneNo(rs.getLong("ADMINPHONENO"));
-			admin.setAdminEmailId(rs.getString("ADMINEMAILID"));
-			admin.setAdminAddress(rs.getString("ADMINADDRESS"));
-			admin.setAdminPassword(rs.getString("ADMINPASSWORD"));	
-			return admin;
+	public Admin search(String aid) throws SQLException, ClassNotFoundException, IOException {
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory factory=cfg.buildSessionFactory();
+		Session session=factory.openSession();
+		Transaction tx=null;
+		Admin admin=null;
+		try{
+			tx=session.beginTransaction();
+			admin=(Admin)session.get(Admin.class,aid);
+			tx.commit();
 		}
-
-		con.close();
-		return null;
+	catch(Exception ex){
+		tx.rollback();
 	}
-
+	finally{
+		session.close();
+		}
+		return admin;
+	}
 	//delete function to delete Admin
 	@Override
-	public int delete(String uid) throws SQLException, ClassNotFoundException, IOException {
+	public int delete(String aid) throws SQLException, ClassNotFoundException, IOException {
+		cfg.configure("hibernate.cfg.xml");
 		
-		int row;
-		Connection con=DbConnection.dbConnection();
-		PreparedStatement pst=con.prepareStatement("delete from admin where ADMINID=?");
-		pst.setString(1, uid);
-		row=pst.executeUpdate();
-		con.close();
-		return row;
+		SessionFactory factory=cfg.buildSessionFactory();
+		Session session=factory.openSession();
+		Transaction tx=null;
+		
+		
+		try{
+			tx=session.beginTransaction();
+			Admin admin=(Admin)session.get(Admin.class,aid);
+			session.delete(admin);
+			tx.commit();
+			session.close();
+			return 1;
+	}
+	catch(Exception ex){
+		tx.rollback();
+		session.close();
+	}
+		return 0;
 	}
 
 	//update function to update the admin details
 		
 	@Override
-	public int update(String uid, Admin admin) throws SQLException, ClassNotFoundException, IOException {
-		int row;
-		Connection con=DbConnection.dbConnection();
-		PreparedStatement pst=con.prepareStatement("update admin set ADMINNAME=?,  ADMINPHONENO=?, 	ADMINEMAILID=?, ADMINADDRESS=?,ADMINPASSWORD=? where ADMINID	=?");
-		pst.setString(1, admin.getAdminName());
-		pst.setLong(2, admin.getAdminPhoneNo());
-		pst.setString(3, admin.getAdminEmailId());
-		pst.setString(4, admin.getAdminAddress());
-		pst.setString(5, admin.getAdminPassword());
-		pst.setString(6, uid);
-		row=pst.executeUpdate();
-		return row;
+	public int update(String aid, Admin admin) throws SQLException, ClassNotFoundException, IOException {
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory factory=cfg.buildSessionFactory();
+		Session session=factory.openSession();
+		Transaction tx=null;
+		try{
+			tx=session.beginTransaction();
+			Admin oldAdmin=(Admin)session.get(Admin.class,aid);
+			session.update(admin);
+			tx.commit();
+			session.close();
+			return 1;
+	}
+	catch(Exception ex){
+		tx.rollback();
+		session.close();
+	}
+		return 0;
 		
 	}
-
-	
 }
